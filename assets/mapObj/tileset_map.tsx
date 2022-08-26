494deae6971f2a4a18e951f73104d58c9a532cf9
ricardo <pelino.rick@gmail.com>
2022-08-25 23:55:52 -0300

    Criando projeto, iniciando mapa

---
+++ assets/mapObj/tileset_map.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="tileset_map" tilewidth="16" tileheight="16" tilecount="1330" columns="35">
+ <image source="map.png" width="560" height="620"/>
+ <tile id="35">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.00116" y="0.0930503" width="1.16313" height="15.5859"/>
+   <object id="3" x="14.9811" y="-0.0465252" width="1.07008" height="16.0512"/>
+  </objectgroup>
+ </tile>
+ <tile id="72">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="11.9104" y="11.957" width="1.02355" height="3.81506"/>
+   <object id="4" x="12.0035" y="12.0035" width="4.28031" height="1.07008"/>
+   <object id="5" x="13.911" y="19.2149"/>
+  </objectgroup>
+ </tile>
+ <tile id="293">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.93225" y="6.93225" width="1.1166" height="9.0724"/>
+   <object id="2" x="6.93225" y="6.88572" width="9.25851" height="1.07008"/>
+  </objectgroup>
+ </tile>
+ <tile id="294">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0930503" y="6.93225" width="15.9116" height="1.16313"/>
+  </objectgroup>
+ </tile>
+</tileset>
